7c73000c1852409e17ae9d4cf886888ffa394e26
dyprojectmusic <166282177+dyprojectmusic@users.noreply.github.com>
2024-04-28 19:59:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,180 +1,137 @@
-new Vue({
-  el: "#app",
-  data() {
-    return {
-      audio: null,
-      circleLeft: null,
-      barWidth: null,
-      duration: null,
-      currentTime: null,
-      isTimerPlaying: false,
-      tracks: [
-      
-              {
-          name: "chicago freestyle",
-          artist: "drake",
-          cover: "cover/cover1.jpg",
-          source: "sound/Drake.mp3",
-          url: "",
-          favorited: false
-        },
-                  {
-          name: "smack that (slowed n reverb)",
-          artist: "akon",
-          cover: "cover/cover2.jpg",
-          source: "sound/smack.mp3",
-          url: "",
-          favorited: true
-        },
-                          {
-          name: "2 days into college",
-          artist: "aimee carty",
-          cover: "cover/cover3.jpg",
-          source: "sound/days.mp3",
-          url: "",
-          favorited: true
-        },
-                          {
-          name: "whats the move",
-          artist: "the kid laroi",
-          cover: "cover/cover4.jpg",
-          source: "sound/laroi.mp3",
-          url: "",
-          favorited: true
-        },
-                          {
-          name: "505",
-          artist: "-Arctic Monkeys",
-          cover: "cover/cover5.jpg",
-          source: "sound/505.mp3",
-          url: "",
-          favorited: true
-        },
-      ],
-      currentTrack: null,
-      currentTrackIndex: 0,
-      transitionName: null
-    };
-  },
-  methods: {
-    play() {
-      if (this.audio.paused) {
-        this.audio.play();
-        this.isTimerPlaying = true;
-      } else {
-        this.audio.pause();
-        this.isTimerPlaying = false;
-      }
-    },
-    generateTime() {
-      let width = (100 / this.audio.duration) * this.audio.currentTime;
-      this.barWidth = width + "%";
-      this.circleLeft = width + "%";
-      let durmin = Math.floor(this.audio.duration / 60);
-      let dursec = Math.floor(this.audio.duration - durmin * 60);
-      let curmin = Math.floor(this.audio.currentTime / 60);
-      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
-      if (durmin < 10) {
-        durmin = "0" + durmin;
-      }
-      if (dursec < 10) {
-        dursec = "0" + dursec;
-      }
-      if (curmin < 10) {
-        curmin = "0" + curmin;
-      }
-      if (cursec < 10) {
-        cursec = "0" + cursec;
-      }
-      this.duration = durmin + ":" + dursec;
-      this.currentTime = curmin + ":" + cursec;
-    },
-    updateBar(x) {
-      let progress = this.$refs.progress;
-      let maxduration = this.audio.duration;
-      let position = x - progress.offsetLeft;
-      let percentage = (100 * position) / progress.offsetWidth;
-      if (percentage > 100) {
-        percentage = 100;
-      }
-      if (percentage < 0) {
-        percentage = 0;
-      }
-      this.barWidth = percentage + "%";
-      this.circleLeft = percentage + "%";
-      this.audio.currentTime = (maxduration * percentage) / 100;
-      this.audio.play();
-    },
-    clickProgress(e) {
-      this.isTimerPlaying = true;
-      this.audio.pause();
-      this.updateBar(e.pageX);
-    },
-    prevTrack() {
-      this.transitionName = "scale-in";
-      this.isShowCover = false;
-      if (this.currentTrackIndex > 0) {
-        this.currentTrackIndex--;
-      } else {
-        this.currentTrackIndex = this.tracks.length - 1;
-      }
-      this.currentTrack = this.tracks[this.currentTrackIndex];
-      this.resetPlayer();
-    },
-    nextTrack() {
-      this.transitionName = "scale-out";
-      this.isShowCover = false;
-      if (this.currentTrackIndex < this.tracks.length - 1) {
-        this.currentTrackIndex++;
-      } else {
-        this.currentTrackIndex = 0;
-      }
-      this.currentTrack = this.tracks[this.currentTrackIndex];
-      this.resetPlayer();
-    },
-    resetPlayer() {
-      this.barWidth = 0;
-      this.circleLeft = 0;
-      this.audio.currentTime = 0;
-      this.audio.src = this.currentTrack.source;
-      setTimeout(() => {
-        if(this.isTimerPlaying) {
-          this.audio.play();
-        } else {
-          this.audio.pause();
-        }
-      }, 300);
-    },
-    favorite() {
-      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
-        this.currentTrackIndex
-      ].favorited;
-    }
-  },
-  created() {
-    let vm = this;
-    this.currentTrack = this.tracks[0];
-    this.audio = new Audio();
-    this.audio.src = this.currentTrack.source;
-    this.audio.ontimeupdate = function() {
-      vm.generateTime();
-    };
-    this.audio.onloadedmetadata = function() {
-      vm.generateTime();
-    };
-    this.audio.onended = function() {
-      vm.nextTrack();
-      this.isTimerPlaying = true;
-    };
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
 
-    // this is optional (for preload covers)
-    for (let index = 0; index < this.tracks.length; index++) {
-      const element = this.tracks[index];
-      let link = document.createElement('link');
-      link.rel = "prefetch";
-      link.href = element.cover;
-      link.as = "image"
-      document.head.appendChild(link)
-    }
+const songs = [
+  {
+    title: "505",
+    name: "Monkeys",
+    source: "https://github.com/dyprojectmusic/dyprojectmusic.github.io/raw/main/sound/505.mp3",
+  },
+  {
+    title: "Runtuh",
+    name: "Feby, Putri",
+    source: "sound/runtuh.mp3",
+  },
+  {
+    title: "2 Days Into College",
+    name: "aimee carty",
+    source:
+      "https://github.com/dyprojectmusic/dyprojectmusic.github.io/raw/main/sound/days.mp3",
+  },
+  {
+    title: "Smack That",
+    name: "AKON",
+    source:
+      "https://github.com/dyprojectmusic/dyprojectmusic.github.io/raw/main/sound/smack.mp3",
+  },
+  {
+    title: "chicago freestyle",
+    name: "Drake",
+    source:
+      "https://github.com/dyprojectmusic/dyprojectmusic.github.io/raw/main/sound/Drake.mp3",
+  },
+
+  {
+    title: "whats the move",
+    name: "Laroi",
+    source:
+      "https://github.com/dyprojectmusic/dyprojectmusic.github.io/blob/main/sound/laroi.mp3",
+  },
+  {
+    title: "Delicate",
+    name: "Taylor Swift",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Taylor-Swift-Delicate.mp3",
+  },
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", function () {});
+}
+
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
   }
+});
+
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
 });
